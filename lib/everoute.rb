e9bc982a3d80b288c0de924c265c20afa91279a8
Daniel Trierweiler <trierweiler@dievision.de>
2011-12-29 10:26:29 +0100

    Added find_tradehubs and fixed some small bugs.

--- lib/everoute.rb
+++ lib/everoute.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 require 'nokogiri'
 require 'mechanize'
+require 'evesystem'
 
 class Everoute
 
   BASE_URL = 'http://evemaps.dotlan.net/route/'
+  TRADEHUBS = %w(Jita Rens Hek Amarr Dodixie)
 
   attr_accessor :route
 
@@ -12,16 +14,15 @@ class Everoute
     @agent = Mechanize.new
   end
 
-
   def travel(system_arr)
-    sys_string = system_arr[0]
+    sys_string = system_arr[0].clone
     system_arr.each_with_index do |sys, i|
       next if i == 0
       sys_string << ":" + sys
     end
     page = @agent.get BASE_URL+sys_string
     arr = page.parser.xpath('//html/body/div/div/div[2]/div[2]/div/div/table/tr')
-    @route = []
+    route = []
     arr.each_with_index do |a, i|
       next if i == 0
       scan = [i]
@@ -30,6 +31,16 @@ class Everoute
       route << Evesystem.new(scan)
     end
     route
+  end
+
+  def find_tradehubs(start_sys)
+    output = ""
+    TRADEHUBS.each do |hub|      
+      temp_arr = [start_sys, hub]
+      route = travel(temp_arr)
+      output << "#{hub}: #{route.size}  "
+    end
+    output
   end
 
 end
